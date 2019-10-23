05a880d1091ad4eeba75078379f34a7e91e2692e
Kiara Anderson <kiara.m.anderson@gmail.com>
2019-10-23 17:58:36 -0400

    added notes on jumpman

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,17 @@
 
 // Complete this algo
 const minJumps = arr => {
+  //max jump size = val of current position
+  //current position starts as 0
+  //worth of each = val + (current position - position)
+  //choose jump size to place on position with max worth
+  let jump = arr[0]
 
+  let worthArr = []
+  for (let i = 1; i <= jump; i++) {
+    //position in worthArr + 1 = jump size
+    worthArr.push(arr[0 + i] + i)
+  }
 };
 
 module.exports = minJumps
