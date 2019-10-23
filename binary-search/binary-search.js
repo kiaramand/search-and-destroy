b4b550247fcdbb31e9a2c9a0f7ad71f87aca76da
Meredyth Houpos <meredyth.houpos@gmail.com>
2019-10-23 17:26:41 -0400

    looper and binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,31 @@
 'use strict';
 
+const split = (array) => {
+	const num = Math.floor(array.length / 2)
+	const firstHalf = array.slice(0, num)
+	const lastHalf = array.slice(num + 1)
+
+	return [firstHalf, lastHalf]
+}
 // Complete this algo
 const binarySearch = (array, target) => {
-	
-};
+	if (array.length > 1) {
+		const splitArr = split(array)
+		const firstHalf = splitArr[0]
+		const secondHalf = splitArr[1]
+		if (target === firstHalf[-1]) {
+			return true;
+		}
+		else if (target > firstHalf[-1]) {
+			return binarySearch(secondHalf, target)
+		} else {
+			return binarySearch(firstHalf, target)
+		}
+	} else {
+		return (array[0] === target)
+	}
+
+};``
 
 /*
 	EXTRA CREDIT:
